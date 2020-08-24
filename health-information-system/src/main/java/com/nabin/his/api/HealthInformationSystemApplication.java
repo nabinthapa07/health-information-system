baf8b9e80740c9bb46e3dd44eb6fff8c128fb325
nabinthapa07 <nabin.thapant51@gmail.com>
2020-08-23 22:03:11 -0500

    added project skeletal

---
+++ health-information-system/src/main/java/com/nabin/his/api/HealthInformationSystemApplication.java
@@ -0,0 +1,13 @@
+package com.nabin.his.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HealthInformationSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HealthInformationSystemApplication.class, args);
+	}
+
+}
---
+++ health-information-system/src/test/java/com/nabin/his/api/HealthInformationSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nabin.his.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthInformationSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
